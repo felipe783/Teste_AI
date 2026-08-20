60d393c95fcc6365192b8ff0c6ab1fadf050ec24
felipe_fedora <felipebolt2007@gmail.com>
2026-08-20 15:56:45 -0300

    train_genetic OK

---
+++ GA/train.genetic.py
@@ -0,0 +1,88 @@
+import random
+from SnakeGameAI.GA.agent import Agent
+from SnakeGameAI.DQN.Helper import plot
+
+POPULATION_SIZE = 80 
+GENERATIONS = 500
+ELITE_FRACTION = 0.10
+TOURNAMENT_SIZE = 5
+
+# Selecao
+
+def tournament_selection(population): 
+    # Pega 5 individuos aleatorios da populacao e retorna o melhor
+    competitors = random.sample(population, TOURNAMENT_SIZE)
+    return max(competitors, key=lambda agent: agent.fitness)
+
+def next_generation(population):
+    # Melhor para o pior
+    population.sort(key=lambda agent: agent.fitness, reverse=True)
+
+    # Numero de individuos que serao mantidos na proxima geracao
+    # Esta logica usa o Elitismo para garantir que a boa genetica nao seja perdidada
+    n_elite = max(1, int(ELITE_FRACTION * POPULATION_SIZE))
+    new_population = [agent.clone() for agent in population[:n_elite]]
+
+    while len(new_population) < POPULATION_SIZE:
+        # Pega 2 pais para o Crossover
+        parent_a = tournament_selection(population)
+        parent_b = tournament_selection(population)
+        child = Agent.crossover(parent_a, parent_b)
+        child.mutate() 
+        new_population.append(child) # 8 Elites e 72 Filhos
+
+    return new_population
+
+def train():
+    plot_scores = []
+    plot_mean_scores = []
+    total_score = 0
+    record = 0
+    start_gen = 0
+
+    seed_agent, start_gen, record = Agent.load_checkpoint()
+    population = [Agent() for _ in range(POPULATION_SIZE)]
+    if seed_agent is not None:
+        population[0] = seed_agent
+
+    generation = start_gem
+
+    try:
+        for generation in range(start_gen, start_gen + GENERATIONS):
+            gen_best_score = 0
+
+            for agent in population:
+                _, score = agent.play_and_evaluate()
+                gen_best_score = max(gen_best_score, score)
+
+                plot_scores.append(score)
+                total_score += score
+                mean_score = total_score /  len(plot_scores)
+                plot_mean_scores.append(mean_score)
+
+            best_in_gen = max(population, key=lambda a: a.fitness)
+
+            if best_in_gen.score > record:
+                record = best_in_gen.score
+                print(f'Novo recorde: {record} (geração {generation})')
+                best_in_gen.save_checkpoint(generation, record)
+
+            print(
+                'Geração:', generation,
+                'Melhor da geração:', gen_best_score,
+                'Recorde geral:', record,
+                'Fitness top:', round(best_in_gen.fitness, 1)
+            )
+
+            plot(plot_scores, plot_mean_scores)
+            population = next_generation(population)
+
+    except KeyboardInterrupt:
+        print("\nTreinamento interrompido.")                
+        best_in_gen = max(population, key=lambda a: a.fitness)
+        best_in_gen.save_checkpoint(generation, record)
+        print("Melhor Agent salvo!")
+        print("Pode fechar o programa.")
+
+if __name__ == "__main__":
+    train()
