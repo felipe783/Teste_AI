2dd55008ef9dd2194227e093d472d98adda997ea
felipe_fedora <felipebolt2007@gmail.com>
2026-09-02 07:50:17 -0300

    Mudaca no Objeto Cell

--- Sudoku/Class/Cell.py
+++ Sudoku/Class/Cell.py
@@ -1,13 +1,11 @@
 from .States import State
 
 class Cell:
-    def __init__(self, value=0): # Quando Inicializa o Objeto
+    
+    def __init__(self, value=0, fixed=False): # Quando Inicializa o Objeto
         self.value = value 
-
-        if value == 0:
-            self.state = State.EMPTY
-        else:
-            self.state = State.FILLED
-
+        self.fixed = fixed
+        self.state = State.EMPTY if value == 0 else State.FILLED
+    
     def __str__(self): # toString do JAVA
         return str(self.value) if self.state == State.FILLED else " "
--- Sudoku/Core/BoardPatterns.py
+++ Sudoku/Core/BoardPatterns.py
@@ -1,45 +1,45 @@
 from Class.Cell import Cell
 
 easyPattern = [
-    [Cell(5), Cell(3), Cell(), Cell(), Cell(7), Cell(), Cell(), Cell(), Cell()],
-    [Cell(6), Cell(), Cell(), Cell(1), Cell(9), Cell(5), Cell(), Cell(), Cell()],
-    [Cell(), Cell(9), Cell(8), Cell(), Cell(), Cell(), Cell(), Cell(6), Cell()],
+    [Cell(5,True), Cell(3,True), Cell(), Cell(), Cell(7,True), Cell(), Cell(), Cell(), Cell()],
+    [Cell(6,True), Cell(), Cell(), Cell(1,True), Cell(9,True), Cell(5,True), Cell(), Cell(), Cell()],
+    [Cell(), Cell(9,True), Cell(8,True), Cell(), Cell(), Cell(), Cell(), Cell(6,True), Cell()],
 
-    [Cell(8), Cell(), Cell(), Cell(), Cell(6), Cell(), Cell(), Cell(), Cell(3)],
-    [Cell(4), Cell(), Cell(), Cell(8), Cell(), Cell(3), Cell(), Cell(), Cell(1)],
-    [Cell(7), Cell(), Cell(), Cell(), Cell(2), Cell(), Cell(), Cell(), Cell(6)],
+    [Cell(8,True), Cell(), Cell(), Cell(), Cell(6,True), Cell(), Cell(), Cell(), Cell(3,True)],
+    [Cell(4,True),Cell(),Cell(),Cell(8,True),Cell(),Cell(3,True),Cell(),Cell(),Cell(1,True)],
+    [Cell(7,True), Cell(), Cell(), Cell(), Cell(2,True), Cell(), Cell(), Cell(), Cell(6,True)],
 
-    [Cell(), Cell(6), Cell(), Cell(), Cell(), Cell(), Cell(2), Cell(8), Cell()],
-    [Cell(), Cell(), Cell(), Cell(4), Cell(1), Cell(9), Cell(), Cell(), Cell(5)],
-    [Cell(), Cell(), Cell(), Cell(), Cell(8), Cell(), Cell(), Cell(7), Cell(9)]
+    [Cell(), Cell(6,True), Cell(), Cell(), Cell(), Cell(), Cell(2,True), Cell(8,True), Cell()],
+    [Cell(), Cell(), Cell(), Cell(4,True), Cell(1,True), Cell(9,True), Cell(), Cell(), Cell(5,True)],
+    [Cell(), Cell(), Cell(), Cell(), Cell(8,True),Cell(),Cell(),Cell(7,True),Cell(9,True)]
 ]
 
 mediumPattern = [
-    [Cell(8), Cell(), Cell(9), Cell(2), Cell(), Cell(), Cell(), Cell(), Cell(4)],
-    [Cell(), Cell(), Cell(), Cell(), Cell(), Cell(4), Cell(), Cell(), Cell()],
-    [Cell(3), Cell(), Cell(), Cell(6), Cell(), Cell(1), Cell(), Cell(), Cell()],
+    [Cell(8,True), Cell(), Cell(9,True), Cell(2,True), Cell(), Cell(), Cell(), Cell(), Cell(4,True)],
+    [Cell(), Cell(), Cell(), Cell(), Cell(), Cell(4,True), Cell(), Cell(), Cell()],
+    [Cell(3,True), Cell(), Cell(), Cell(6,True), Cell(), Cell(1,True), Cell(), Cell(), Cell()],
 
-    [Cell(4), Cell(9), Cell(1), Cell(), Cell(6), Cell(7), Cell(), Cell(), Cell(3)],
-    [Cell(6), Cell(), Cell(), Cell(), Cell(), Cell(9), Cell(8), Cell(1), Cell()],
-    [Cell(), Cell(), Cell(), Cell(), Cell(2), Cell(3), Cell(), Cell(9), Cell(6)],
+    [Cell(4,True), Cell(9,True), Cell(1,True), Cell(), Cell(6,True), Cell(7,True),Cell(),Cell(),Cell(3,True)],
+    [Cell(6,True),Cell(),Cell(),Cell(),Cell(),Cell(9,True),Cell(8,True),Cell(1,True),Cell()],
+    [Cell(), Cell(), Cell(), Cell(), Cell(2,True), Cell(3,True), Cell(), Cell(9,True), Cell(6,True  )],
 
-    [Cell(1), Cell(), Cell(), Cell(5), Cell(), Cell(), Cell(), Cell(), Cell(9)],
-    [Cell(), Cell(6), Cell(4), Cell(3), Cell(), Cell(2), Cell(), Cell(), Cell(5)],
-    [Cell(), Cell(8), Cell(3), Cell(9), Cell(7), Cell(), Cell(), Cell(), Cell()]
+    [Cell(1,True), Cell(), Cell(), Cell(5,True), Cell(), Cell(), Cell(), Cell(), Cell(9,True)],
+    [Cell(), Cell(6,True), Cell(4,True), Cell(3,True), Cell(), Cell(2,True), Cell(), Cell(), Cell(5,True)],
+    [Cell(), Cell(8,True), Cell(3,True), Cell(9,True), Cell(7,True), Cell(), Cell(), Cell(), Cell()]
 ]
 
 hardPattern = [
-    [Cell(), Cell(), Cell(), Cell(), Cell(5), Cell(), Cell(), Cell(), Cell(4)],
-    [Cell(2), Cell(1), Cell(3), Cell(), Cell(), Cell(4), Cell(), Cell(), Cell()],
-    [Cell(5), Cell(), Cell(), Cell(3), Cell(7), Cell(), Cell(8), Cell(), Cell(6)],
+    [Cell(), Cell(), Cell(), Cell(), Cell(5,True), Cell(), Cell(),Cell(),Cell(4,True)],
+    [Cell(2,True),Cell(1,True),Cell(3,True),Cell(),Cell(),Cell(4,True),Cell(),Cell(),Cell()],
+    [Cell(5,True),Cell(),Cell(),Cell(3,True),Cell(7,True),Cell(),Cell(8,True),Cell(),Cell(6,True)]
 
-    [Cell(), Cell(), Cell(6), Cell(), Cell(), Cell(), Cell(7), Cell(8), Cell()],
-    [Cell(), Cell(), Cell(), Cell(1), Cell(), Cell(), Cell(6), Cell(5), Cell()],
-    [Cell(), Cell(), Cell(), Cell(), Cell(), Cell(), Cell(), Cell(), Cell(1)],
+    [Cell(), Cell(), Cell(6,True), Cell(), Cell(), Cell(), Cell(7,True), Cell(8,True), Cell()],
+    [Cell(), Cell(), Cell(), Cell(1,True), Cell(), Cell(), Cell(6,True), Cell(5,True), Cell()],
+    [Cell(), Cell(), Cell(), Cell(), Cell(), Cell(), Cell(), Cell(), Cell(1,True)],
 
-    [Cell(7), Cell(8), Cell(), Cell(), Cell(1), Cell(), Cell(9), Cell(), Cell()],
-    [Cell(6), Cell(), Cell(), Cell(), Cell(), Cell(2), Cell(), Cell(), Cell()],
-    [Cell(), Cell(2), Cell(), Cell(), Cell(9), Cell(), Cell(), Cell(), Cell(8)]
+    [Cell(7,True), Cell(8,True), Cell(), Cell(), Cell(1,True),Cell(),Cell(9,True),Cell(),Cell()],
+    [Cell(6,True), Cell(),Cell(),Cell(),Cell(),Cell(2,True),Cell(),Cell(),Cell()],
+    [Cell(),Cell(2,True),Cell(),Cell(),Cell(9,True),Cell(),Cell(),Cell(),Cell(8,True)]
 ]
 
 victoriousBoard = [
