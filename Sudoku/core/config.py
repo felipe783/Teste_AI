58c54617d3c55daa4099f6dcb3be8ff6df8c45bf
felipe_fedora <felipebolt2007@gmail.com>
2026-09-18 12:37:39 -0300

    Sudoku samurai

--- Sudoku/core/config.py
+++ Sudoku/core/config.py
@@ -4,7 +4,20 @@ SAMURAI_NUMBERS_EASY = 148
 SAMURAI_NUMBERS_MEDIUM = 111
 SAMURAI_NUMBERS_HARD = 85
 
+SAMURAI_CONFIG = [
+    ("SUDOKU_A", (0, 12)),   # Linha inicial, coluna inicial
+    ("SUDOKU_B", (6, 18)),
+    ("SUDOKU_C", (6, 6)),
+    ("SUDOKU_D", (12, 0)),
+    ("SUDOKU_E", (12, 12)),
+    ("SUDOKU_F", (12, 24)),
+    ("SUDOKU_G", (18, 6)),
+    ("SUDOKU_H", (18, 18)),
+    ("SUDOKU_I", (24, 12)),
+]
+
 SIZE = 9
-NUMBERS_EASY = 148
-NUMBERS_MEDIUM = 111
-NUMBERS_HARD = 85
+NUMBERS_EASY = 40
+NUMBERS_MEDIUM = 32
+NUMBERS_HARD = 25
+
